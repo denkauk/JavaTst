737e86aca7e8c3f3c687bee2e34897e15dea26cd
dkazachenko <dkazachenko@eisgroup.com>
2026-02-06 22:44:08 +0200

    Phase 2: step 2 logger

--- java-backend/src/main/java/com/developer/test/logging/HttpBodyLoggingFilter.java
+++ java-backend/src/main/java/com/developer/test/logging/HttpBodyLoggingFilter.java
@@ -1,7 +1,4 @@
-/* Copyright © 2026 EIS Group and/or one of its affiliates. All rights reserved. Unpublished work under U.S. copyright laws.
-CONFIDENTIAL AND TRADE SECRET INFORMATION. No portion of this work may be copied, distributed, modified, or incorporated into any other media without EIS Group prior written consent.*/
 package com.developer.test.logging;
-
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
